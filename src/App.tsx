22f41cb12479c93736736d02287c6edc71e8873f
Matheus <matheus.fu.p@hotmail.com>
2023-10-27 14:03:06 -0300

    :art: styled basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import { GlobalStyle } from "./config/GlobalStyle";
+import DefaultTheme from "./config/theme/DefaultTheme";
+import { Home } from "./pages/home/Home";
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <DefaultTheme>
+      <GlobalStyle />
+      <Home />
+    </DefaultTheme>
+  );
 }
 
-export default App
+export default App;
