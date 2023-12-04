4ca734c3b444f53ace297f241f912594333de859
matheus <matheus.fu.p@hotmail.com>
2023-12-04 16:24:37 -0300

    :lipstick: finish styled + responsive navBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 import * as S from './Style'
+
 const NavBar = () => {
+  const [active, setActive] = useState("burger");
+  const showSideBar = () => {
+    if (active === "burger") {
+      setActive("nav-active");
+    } else {
+      setActive("burger");
+    }
+  };
   return (<>
-    <S.Container>
-      <S.SetMenu type='checkbox' role="button"/>
+    <S.Container className={active}>
+      <S.SetMenu type='checkbox' onClick={showSideBar} role="button"/>
+    
+        <S.List className={active}>
+          <S.Item>Sobre</S.Item>
+          <S.Item>Projetos</S.Item>
+          <S.Item>Contato</S.Item>
+        </S.List>
     </S.Container>
   </>
   )
--- src/components/NavBar/Style.ts
+++ src/components/NavBar/Style.ts
@@ -1,4 +1,3 @@
-import { SetMenu } from './Style';
 import styled from "styled-components";
 
 
@@ -6,24 +5,36 @@ import styled from "styled-components";
 export const Container = styled.nav`
 display:flex;
 justify-items: center;
+
 align-items:center;
+flex-direction: column;
 position: fixed;
 top:2%;
 left:20px;
-background-color: white;
-width:40px;
-height:40px;
-border-radius: 50%;
-
+gap:20px;
+width:3rem;
+height:3rem;
+border-radius: 25px;
 background-color:gray;
+transition: all 1s;
+overflow:hidden;
 
 
+&.nav-active{
+  width:25rem;
+}
+@media (max-width: 600px) {
+  &.nav-active{
+  width:22rem;
+}}
 `;
 
 export const SetMenu = styled.input`
-position:absolute;
-top:18px;
-left:10px;
+display:block;
+position:fixed;
+ left: 0;
+ top:10px;
+margin:10px;
   cursor: pointer;
   transition: 
     .5s cubic-bezier(0.19, 1, 0.22, 1);
@@ -36,7 +47,7 @@ left:10px;
   background-color: #fff;
   position: absolute;
   height: 6px;
-  width: 20px;
+  width: 25px;
   transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
   border-radius: 10px;
 }
@@ -50,6 +61,7 @@ left:10px;
 }
 &:checked{
     height:0;
+
 }
 &:checked::before{
     margin-top: 0;
@@ -57,10 +69,34 @@ left:10px;
 }
 &:checked::after{
     margin-top: 0;
-
     transform:  rotate(-225deg) ;
 }
   `;
 
-  export const MenuLabel = styled.label`
-  `;
+export const List = styled.ul`
+display:none;
+flex-direction: row;
+background-color:inherit;
+gap:50px;
+height:100%;
+justify-content:center;
+align-items:center;
+list-style: none;
+&.nav-active{
+  display:flex;
+}
+@media (max-width: 600px) {
+  &.nav-active{
+    gap:30px;
+}}
+`;
+
+export const Item = styled.li`
+background-color:transparent;
+font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+cursor:pointer;
+transition: all .5s;
+&:hover{
+  color: #fff;
+}
+`;
